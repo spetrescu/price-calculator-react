32a831cc9ec1bc3fc4fe7454413a1967bc609df9
Dan Iliescu <dan.iliescu@stud.etti.upb.ro>
2021-12-01 20:50:17 +0200

    fix background

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const App = styled.div`
 //background-color: yellow;
   font-family: poppins-extralight,poppins,sans-serif;
-  background: linear-gradient(180deg, rgba(65,61,61,1) 6%, rgba(58,7,6,1) 64%);
+  //background: linear-gradient(180deg, rgba(65,61,61,1) 6%, rgba(58,7,6,1) 64%);
   //background: green;
   //padding-top:1px;
   //padding-left:1.5rem;
@@ -13,7 +13,7 @@ export const App = styled.div`
   padding-left:1.5rem;
   padding-right:1.5rem;
   width:auto;
-  height:100%;
+  height:100vh;
 `
 export const Title = styled.h2`
 color:white;
